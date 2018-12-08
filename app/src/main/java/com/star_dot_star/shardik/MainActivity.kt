e80e1d4022b6c0319f309a00e09cc155f5b57fad
Sean Kelman <sean.kelman@gmail.com>
2018-12-08 09:27:40 -0500

    MainActivity: add button and onClick handler
    Stubbed for now, implementation to come

--- app/src/main/java/com/star_dot_star/shardik/MainActivity.kt
+++ app/src/main/java/com/star_dot_star/shardik/MainActivity.kt
@@ -2,11 +2,17 @@ package com.star_dot_star.shardik
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_start).setOnClickListener {
+            Toast.makeText(this, "click....", Toast.LENGTH_LONG).show()
+        }
     }
 }
