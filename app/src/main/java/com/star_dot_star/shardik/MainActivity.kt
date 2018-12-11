624c32661b280b7f8276692f332efd5955019aed
Sean Kelman <sean.kelman@gmail.com>
2018-12-10 20:13:00 -0500

    Add VpnService to block hardcoded host names

--- app/src/main/java/com/star_dot_star/shardik/MainActivity.kt
+++ app/src/main/java/com/star_dot_star/shardik/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.star_dot_star.shardik
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.net.VpnService
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
+import com.star_dot_star.shardik.service.AppVpnService
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btn_start).setOnClickListener {
-            Toast.makeText(this, "click....", Toast.LENGTH_LONG).show()
+            val intent = VpnService.prepare(applicationContext)
+            if (intent != null) {
+                startActivityForResult(intent, 0)
+            } else {
+                onActivityResult(0, Activity.RESULT_OK, null)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "Starting VpnService", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, AppVpnService::class.java)
+            startService(intent)
         }
     }
 }
---
+++ app/src/main/java/com/star_dot_star/shardik/service/AppVpnService.kt
@@ -0,0 +1,150 @@
+package com.star_dot_star.shardik.service
+
+import android.app.Service
+import android.content.Intent
+import android.net.VpnService
+import android.os.ParcelFileDescriptor
+import android.util.Log
+import org.pcap4j.packet.DnsPacket
+import org.pcap4j.packet.IpV4Packet
+import org.pcap4j.packet.UdpPacket
+import org.pcap4j.packet.UnknownPacket
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
+
+class AppVpnService : VpnService() {
+    private var thread: Thread? = null
+    private val dnsServer = InetAddress.getByName("8.8.8.8")
+    private var vpnInterface: ParcelFileDescriptor? = null
+    // TODO: just for testing, replace with 'real' list of ad host names
+    private val blacklistedHosts = mutableSetOf("www.reddit.com", "reddit.com", "yahoo.com")
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.i(TAG, "service onStartCommand")
+
+        vpnInterface = Builder()
+            .addAddress("192.168.50.1", 24)
+            .addDnsServer("192.168.50.5")
+            .addRoute("0.0.0.0", 24)
+            .setBlocking(true)
+            .setSession("ShardikVPNService")
+            .establish()
+
+        thread?.interrupt()
+        thread = Thread(Runnable {
+            try {
+                Log.i(TAG, "thread start")
+
+                // VpnService gives us an input stream to read incoming bytes:
+                val inputStream = FileInputStream(vpnInterface?.fileDescriptor)
+
+                val packet = ByteArray(32767)
+
+                // loop until this thread is killed:
+                while (true) {
+                    // Read one packet at a time:
+                    inputStream.read(packet)
+
+                    // Set up output stream
+                    val outputStream = FileOutputStream(vpnInterface?.fileDescriptor)
+
+                    // Protect "allowed" packets from VPN connections
+                    val dnsSocket = DatagramSocket()
+                    protect(dnsSocket)
+
+                    // Handle each packet: either block it for ads or pass through for everything else
+                    handlePacket(packet, dnsSocket, outputStream)
+                }
+            } catch (e: Exception) {
+                // TODO: better error handling
+                e.printStackTrace()
+            } finally {
+                Log.i(TAG, "thread cleanup")
+                vpnInterface?.close()
+                vpnInterface = null
+            }
+        })
+
+        thread?.start()
+
+        return Service.START_STICKY
+    }
+
+    private fun handlePacket(packetBytes: ByteArray, dnsSocket: DatagramSocket, outputStream: FileOutputStream) {
+        try {
+            val packet = IpV4Packet.newPacket(packetBytes, 0, packetBytes.size)
+
+            // We only care about DNS packets:
+            val udpPacket = packet.payload as UdpPacket? ?: return
+            val dnsPacket = udpPacket.payload as DnsPacket? ?: return
+
+            // Read the host name out of DNS packet:
+            val dnsHostName = if (dnsPacket.header.questions.isNotEmpty()) {
+                dnsPacket.header.questions[0].qName.name
+            } else {
+                ""
+            }
+
+            // Check hostname vs list of blacklisted hosts:
+            val response: ByteArray = if (blacklistedHosts.contains(dnsHostName)) {
+                Log.i(TAG, "found host $dnsHostName :: blocking")
+                dnsPacket.rawData
+            } else {
+                Log.i(TAG, "found host $dnsHostName :: passthrough")
+                val rawPacketData = udpPacket.payload.rawData
+                val outPacket = DatagramPacket(rawPacketData, 0, rawPacketData.size, dnsServer, 53)
+
+                dnsSocket.send(outPacket)
+
+                val datagramData = ByteArray(1024)
+                val replyPacket = DatagramPacket(datagramData, datagramData.size)
+                dnsSocket.receive(replyPacket)
+                datagramData
+            }
+
+            // Construct the outgoing packet
+            val ipOutPacket = IpV4Packet.Builder(packet)
+                .srcAddr(packet.header.dstAddr)
+                .dstAddr(packet.header.srcAddr)
+                .correctChecksumAtBuild(true)
+                .correctLengthAtBuild(true)
+                .payloadBuilder(
+                    UdpPacket.Builder(udpPacket)
+                        .srcPort(udpPacket.header.dstPort)
+                        .dstPort(udpPacket.header.srcPort)
+                        .srcAddr(packet.header.dstAddr)
+                        .dstAddr(packet.header.srcAddr)
+                        .correctChecksumAtBuild(true)
+                        .correctLengthAtBuild(true)
+                        .payloadBuilder(
+                            UnknownPacket.Builder()
+                                .rawData(response)
+                        )
+                ).build()
+
+            // Forward the outgoing packet by writing it to the output stream
+            outputStream.write(ipOutPacket.rawData)
+        } catch (e: Exception) {
+            // TODO: error handling
+            Log.e(TAG, e.message)
+        } finally {
+            dnsSocket.close()
+            outputStream.close()
+        }
+    }
+
+    override fun onDestroy() {
+        Log.i(TAG, "service onDestroy")
+        thread?.interrupt()
+        thread = null
+        stopSelf()
+    }
+
+    companion object {
+        private const val TAG = "AppVpnService"
+    }
+}
